d87cc23b3822563c33595af3a03e71762f23ce43
JeffreyHillCatch <143233955+JeffreyHillCatch@users.noreply.github.com>
2023-08-28 10:42:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ streamlit.header('Breakfast menu')
 streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
-streamlit.text(' 🥑🍞 Avocado Toast')
+streamlit.text(' 🥑🍞 Avocado Ttoast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
